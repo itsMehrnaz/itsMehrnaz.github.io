3d7c8c296382be1a8da3778c333416d243017762
itsMehrnaz <ItsmeMehrnaz@gmail.com>
2026-06-29 02:03:29 +0330

    Replace old site with Astro blog (FA/EN support)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://itsmehrnaz.github.io',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
@@ -30,6 +30,13 @@ export default defineConfig({
 					},
 				],
 			},
+		},
+         
+               {
+			provider: fontProviders.google(),
+			name: 'Vazirmatn',
+			cssVariable: '--font-vazir',
+			fallbacks: ['sans-serif'],
 		},
 	],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -10,6 +10,7 @@ const blog = defineCollection({
 		z.object({
 			title: z.string(),
 			description: z.string(),
+                        lang: z.enum(['fa', 'en']).default('en'),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
